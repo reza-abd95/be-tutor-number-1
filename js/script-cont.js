f96ab0faa34080b643f95d8a59ca5cb160680e86
sam zeinali <saamzeinali@gmail.com>
2023-04-19 14:13:09 -0700

    feat(contact page) : added page

---
+++ js/script-cont.js
@@ -0,0 +1,24 @@
+let valueDisplays = document.querySelectorAll(".number-txt");
+let interval = 1000;
+
+function addheadersteaky() {
+    if ($.documentElement.scrollTop > 400 && $.documentElement.scrollTop < 402 ) {
+        console.log(valueDisplays);
+        valueDisplays.forEach((valueDisplay) => {
+            let startvalue = 0;
+            let endvalue = parseInt(valueDisplay.getAttribute("data-val"));
+            console.log(endvalue);
+            let duration = Math.floor(interval / endvalue);
+            let counter = setInterval(function(){
+                startvalue += 1;
+                valueDisplay.textContent = startvalue;
+                if(startvalue == endvalue) {
+                    clearInterval(counter);
+                }
+            },duration);    
+        
+        })
+    } else {
+        clearInterval(counter);
+    }
+}
